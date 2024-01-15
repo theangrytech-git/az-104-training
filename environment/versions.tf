64befbed456ded0cec0ecef813cb45d5c2cd98c5
theangrytech-git <theangrytech@outlook.com>
2024-01-15 23:06:02 +0000

    change feature flag to allow RG deletion if contains resources

--- environment/versions.tf
+++ environment/versions.tf
@@ -17,6 +17,9 @@ provider "azurerm" {
       purge_soft_delete_on_destroy = true
       recover_soft_deleted         = true
     }
+    resource_group {
+    prevent_deletion_if_contains_resources = false
+    }
 
   }
 }
