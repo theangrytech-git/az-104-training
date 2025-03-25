5731a5c92925052050d78f2783f8dcf60f19643b
theangrytech-git <theangrytech@outlook.com>
2025-03-25 21:20:12 +0000

    Create secrets.tf
    test secret terraform file
    
    Signed-off-by: theangrytech-git <theangrytech@outlook.com>

---
+++ secrets.tf
@@ -0,0 +1,15 @@
+variable "db_password1" {
+  default = "SuperSecretqweq123!"
+}
+
+variable "db_password2" {
+  default = "S3kdi4uperSecret123!"
+}
+
+variable "db_password3" {
+  default = "Supwsgvhu25erSecret123!"
+}
+
+variable "db_password4" {
+  default = "Super48364Secret123!"
+}
